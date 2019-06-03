8023eae965926aec9c4bb19e2d5d8aa495790bce
JacobGallagher <47995251+JacobGallagher@users.noreply.github.com>
2019-06-04 11:53:28 +1200

    NavMesh/Initial Commit

---
+++ IGRAAMV2/IGRAAM/Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //Variables
+    public float movementSpeed; 
+
+    //Methods
+    void Update()
+    {
+        //Player Movement
+        if (Input.GetKey(KeyCode.W)||Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * movementSpeed * Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * movementSpeed * Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector3.back * movementSpeed * Time.deltaTime);
+        }
+    }
+}
