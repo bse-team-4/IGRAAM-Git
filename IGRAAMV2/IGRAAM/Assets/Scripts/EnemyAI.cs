bf5184dac2c0288bd8d7f7a341150ef35341b983
harsh2048 <46461982+harsh2048@users.noreply.github.com>
2019-06-18 03:39:13 +0700

    finished the AI

---
+++ IGRAAMV2/IGRAAM/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    public Transform player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector2.Distance(player.position, this.transform.position) < 5)
+        {
+            Vector3 direction = player.position - this.transform.position;
+            direction.y = 0;
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), 0.1f);
+
+
+            if(direction.magnitude > 5)
+            {
+                this.transform.Translate(0, 0, 0.05f);
+            }
+        }
+    }
+}
