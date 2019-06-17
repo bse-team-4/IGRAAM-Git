df55f0006548e067adb72fc39e9ef9ea4a772cb1
harsh2048 <harshit150799@gmail.com>
2019-06-18 11:53:25 +1200

    change #2

--- IGRAAMV2/IGRAAM/Assets/Scripts/EnemyAI.cs
+++ IGRAAMV2/IGRAAM/Assets/Scripts/EnemyAI.cs
@@ -15,7 +15,7 @@ public class EnemyAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Vector2.Distance(player.position, this.transform.position) < 5)
+        if(Vector2.Distance(player.position, this.transform.position) < 10)
         {
             Vector3 direction = player.position - this.transform.position;
             direction.y = 0;
---
+++ IGRAAMV2/IGRAAM/Assets/Scripts/SEnemyAI.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SEnemyAI : MonoBehaviour
+{
+
+    Transform playerTransform;
+    UnityEngine.AI.NavMeshAgent myNavmesh;
+
+
+    public float checkRate = 0.0f;
+
+    float nextCheck;
+    void Start()
+    {
+        if (GameObject.FindGameObjectWithTag("Player").activeInHierarchy)
+            playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+
+        myNavmesh = gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > nextCheck)
+        {
+            nextCheck = Time.time + checkRate;
+            FollowPlayer();
+        }
+            
+    }
+
+    void FollowPlayer()
+    {
+        myNavmesh.transform.LookAt(playerTransform);
+        myNavmesh.destination = playerTransform.position;
+    }
+}
